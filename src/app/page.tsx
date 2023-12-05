875153aefbf7ac2df9996368584281df650131f7
HamidFt <hamidfattahi.a@gmail.com>
2023-12-05 09:05:56 +0330

    some basic tests have been written

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,43 +1,10 @@
 import Image from "next/image";
 import { Button } from "./_components/button/button";
+import HomeHeroSection from "./_components/home-hero-section/home-hero-section";
 export default function Home() {
   return (
     <>
-      <section className="bg-hero-pattern mt-5 xl:mt-20 xl:bg-left">
-        <div className="container flex flex-col-reverse items-center xl:flex-row">
-          <div className="flex flex-col gap-5 mt-12 pb-5 text-center xl:text-right">
-            <h3 className="text-xl dark:text-info xl:text-2xl">
-              خوش آمدید به ...
-            </h3>
-            <h1 className="text-3xl font-black gradient lg:text-5xl xl:text-5xl">
-              مسیر سعود به قله های برنامه نویسی
-            </h1>
-            <p>هرجای مسیر برنامه نویسی که باشی هواتو داریم</p>
-
-            <div className="my-5 flex gap-4">
-              <Button variant="primary" size="large">
-                دوره های ری اکت و نکست
-              </Button>
-              <Button variant="neutral" size="large">
-                مشاوره برنامه نویسی
-              </Button>
-            </div>
-            <Image
-              className="grayscale mt-4 opacity-70 m-auto xl:m-0"
-              src="/images/frameworks.png"
-              width={412}
-              height={39}
-              alt="FrameWorks"
-            />
-          </div>
-          <Image
-            src="/images/programmer-landing.svg"
-            width={702}
-            height={521}
-            alt="classbon"
-          />
-        </div>
-      </section>
+      <HomeHeroSection />
       <div className="container">
         <Button variant="info" size="large" shape="wide" animatedIcon={true}>
           ثبت نام
